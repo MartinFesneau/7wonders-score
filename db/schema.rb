766145dc5f853016f14fcb6bb00d3ee0fe9a4716
Martin Fesneau <martin.fesneau@gmail.com>
2021-08-28 16:23:38 +0200

    model Board

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_141442) do
+ActiveRecord::Schema.define(version: 2021_08_28_142259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "side", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -22,11 +29,8 @@ ActiveRecord::Schema.define(version: 2021_08_28_141442) do
 
   create_table "players", force: :cascade do |t|
     t.string "name", null: false
-    t.bigint "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["game_id"], name: "index_players_on_game_id"
   end
 
-  add_foreign_key "players", "games"
 end
